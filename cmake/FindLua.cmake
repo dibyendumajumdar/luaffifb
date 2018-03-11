2f8e55363dfd12845ddb968b0553a81d59836c90
Dibyendu Majumdar <noemail>
2018-03-11 00:02:01 +0000

    also install test script

--- cmake/FindLua.cmake
+++ cmake/FindLua.cmake
@@ -14,6 +14,13 @@ if (USE_LUA53)
     ~/lua53/lib
   )
 
+  find_program(LUA_EXE
+    NAMES lua
+    PATHS
+    c:/Software/lua53/bin
+    ~/lua53/bin
+  )
+
 else()
 
   find_path(LUA_INCLUDE_DIR lua.h
@@ -30,8 +37,56 @@ else()
     ~/ravi/lib
   )
 
+  find_program(LUA_EXE
+    NAMES ravi_s
+    PATHS
+    c:/Software/ravi/bin
+    ~/ravi/bin
+  )
+
 endif()
 
-if (NOT WIN32)
-  set( LUA_LIBRARIES "${LUA_LIBRARY};m")
+# LUA_INCDIR - place where lua headers exist
+set(LUA_INCDIR ${LUA_INCLUDE_DIR})
+
+# LIBDIR - LUA_CPATH
+if (WIN32)
+
+  get_filename_component(LIBDIR
+    ${LUA_EXE}
+    DIRECTORY)
+
+else()
+
+  get_filename_component(LIBDIR
+    ${LUA_LIBRARIES}
+    DIRECTORY)
+
 endif()
+
+get_filename_component(LUA_BINDIR
+  ${LUA_EXE}
+  DIRECTORY)
+
+# LUA_LIBDIR - place where lua native libraries exist
+get_filename_component(LUA_LIBDIR
+  ${LUA_LIBRARIES}
+  DIRECTORY
+)
+
+if (NOT WIN32) 
+  set(LUA_LIBRARIES "${LUA_LIBRARIES};m")
+endif()
+
+# LUALIB - the lua library to link against
+set(LUALIB ${LUA_LIBRARIES})
+
+# LUADIR - LUA_PATH
+if (USE_LUA53)
+  set(LUADIR "${LUA_LIBDIR}/../share/lua/5.3")
+else()
+  set(LUADIR "${LUA_LIBDIR}/../share/lua/5.3")
+endif()
+
+set(LUA "${LUA_EXE}")
+
